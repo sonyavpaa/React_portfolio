5a154498ccd4d4636d4019f9bd50f5e3659f52fe
Sonya Vpaa <sonja.vpaa@gmail.com>
2022-09-02 09:29:32 +0300

    updated responsivess

--- src/assets/projectsAPI.js
+++ src/assets/projectsAPI.js
@@ -5,8 +5,7 @@ export const projectsArr = [
     text: "an image portfolio i made with React.js",
     url: <a href="https://www.sonjavainionpaa.fi">sonjavainionpaa.fi</a>,
     src: (
-      <a href="https://github.com/sonyavpaa/React_portfolio.git ">
-        {" "}
+      <a href="https://github.com/sonyavpaa/React_gallery">
         <img
           className="gitHubLogo"
           src={require("../assets/img/GitHub-Mark-Light-32px.png")}
